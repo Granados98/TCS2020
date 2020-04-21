6dcbd8aff8bec6930b0f5a12ffb64f8987c1e8d2
Granados98 <54607906+Granados98@users.noreply.github.com>
2020-04-21 12:45:02 -0500

    nuget

--- Transito Veracruz/Transito Veracruz/Delegacion/Login.xaml.cs
+++ Transito Veracruz/Transito Veracruz/Delegacion/Login.xaml.cs
@@ -23,5 +23,10 @@ namespace Transito_Veracruz.Delegacion
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
