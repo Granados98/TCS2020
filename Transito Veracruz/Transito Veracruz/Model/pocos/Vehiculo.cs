8d33674a7aaa9dcdf43a94eda021855e372e1270
CesarDiaz2712 <cesar_diaz16@outlook.com>
2020-05-07 02:42:20 -0500

    Avance proyecto delegacion
    creacion de clases y conexion a la base de datos

---
+++ Transito Veracruz/Transito Veracruz/Model/pocos/Vehiculo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Transito_Veracruz.Model.pocos
+{
+    class Vehiculo
+    {
+        private Int32 idVehiculo;
+        private String numeroPLacas;
+        private String marca;
+        private String modelo;
+        private String anio;
+        private String color;
+        private String nombreAseguradora;
+        private String numeroPolizaSeguro;
+        private String numeroLicencia;
+
+        public int IdVehiculo { get => idVehiculo; set => idVehiculo = value; }
+        public string NumeroPLacas { get => numeroPLacas; set => numeroPLacas = value; }
+        public string Marca { get => marca; set => marca = value; }
+        public string Modelo { get => modelo; set => modelo = value; }
+        public string Anio { get => anio; set => anio = value; }
+        public string Color { get => color; set => color = value; }
+        public string NombreAseguradora { get => nombreAseguradora; set => nombreAseguradora = value; }
+        public string NumeroPolizaSeguro { get => numeroPolizaSeguro; set => numeroPolizaSeguro = value; }
+        public string NumeroLicencia { get => numeroLicencia; set => numeroLicencia = value; }
+    }
+}
